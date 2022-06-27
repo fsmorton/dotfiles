eb0b5f96c98bda5ac00cc472c5ea8408d1ddea3c
fsmorton <francismorton@tutanota.com>
2022-06-27 11:30:13 -0400

    added fish config

---
+++ fish/config.fish
@@ -0,0 +1,44 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# remove fish startup message
+set -U fish_greeting
+
+# common shortcuts
+
+alias c='clear'
+alias h='history'
+alias bd='cd ..'
+alias lsa='ls -a'
+
+# git
+
+alias gs='git status'
+alias ga='git add'
+alias gaa='git add --all'
+alias gc='git commit'
+alias gp='git push'
+alias gpom='git push origin main'
+
+# pacman & yay
+
+alias pacin='sudo pacman -S'
+alias pacdel='sudo pacman -Rns'
+alias pacdelkd='sudo pacman -R'
+alias pacmirror='sudo pacman -Syy'
+alias yays='yay -S'
+alias yayr='yay -Rns'
+alias yayrkd='yay -R'
+
+# program
+
+alias makeweb='touch index.html main.js style.css'
+alias py='python3'
+alias nv='nvim'
+
+# cowsay
+alias cow='cowsay $(fortune)'
+
+# youtube download
+alias musicd='youtube-dl -x --audio-format mp3'
